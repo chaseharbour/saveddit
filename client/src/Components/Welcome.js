ca9fe3408a78e18d5d783605e962b4cb789a5363
Corey Burkhart <corey.burkhart@bvaccel.com>
2021-03-01 00:01:45 -0800

    feat(welcome): add well wishes
    - also say hi

--- client/src/Components/Welcome.js
+++ client/src/Components/Welcome.js
@@ -44,7 +44,10 @@ const Welcome = (props) => {
                 setAuthenticated={setAuthenticated}
             />
             {!authenticated ? (
-                <h1>Welcome!</h1>
+                <>
+                    <h1>Welcome!</h1>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/aB2yqeD0Nus" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </>
             ) : (
                 <div>
                     <h1>You have logged in successfully!</h1>
