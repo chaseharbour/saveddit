33d681e3a045210c58171995c4b11a97f9ab2af7
chaseharbour <harbourcp@gmail.com>
2021-03-18 18:06:31 -0700

    prog(Dockerfile, docker-compose.yml): Docker set up for deployment.

--- server/routes/auth_routes.js
+++ server/routes/auth_routes.js
@@ -1,64 +1,63 @@
-const dotenv = require('dotenv').config();
-const router = require('express').Router();
-const snoowrap = require('snoowrap');
-const CLIENT_HOME_PAGE_URL = "http://localhost:3000/";
-
-//const userServices = require('../db/services/user');
-
+const dotenv = require("dotenv").config();
+const router = require("express").Router();
+const snoowrap = require("snoowrap");
 
 const {
+  REDIRECT_URI,
+  CLIENT_HOST_ADDRESS = "localhost",
+  CLIENT_PORT,
   CLIENT_ID,
   CLIENT_SECRET,
   REDDIT_USER,
-  REDDIT_PASSWORD
+  REDDIT_PASSWORD,
 } = process.env;
 
+const CLIENT_HOME_PAGE = `http://${CLIENT_HOST_ADDRESS}:${CLIENT_PORT}/`;
 
-router.get('/login/success', (req, res) => {
-
-    if (req.session.userName) {
-        res.json({
-            success: true,
-            message: "User has successfully authenticated",
-            user: req.session.userName,
-            cookies: req.session.cookie
-        });
-
-    }
-});
-
-router.get('/login/failed', (req, res) => {
-    res.status(401).json({
-        success: false,
-        message: "User failed to authenticate."
+router.get("/login/success", (req, res) => {
+  if (req.session.userName) {
+    res.json({
+      success: true,
+      message: "User has successfully authenticated",
+      user: req.session.userName,
+      cookies: req.session.cookie,
     });
+  }
 });
 
-router.get('/logout', (req, res) => {
+router.get("/login/failed", (req, res) => {
+  res.status(401).json({
+    success: false,
+    message: "User failed to authenticate.",
+  });
+});
+
+router.get("/logout", (req, res) => {
   //Sessions not deleted from store but expire after an hour anyways, worth the trouble? TBD...
-  req.session.destroy(err => {
+  req.session.destroy((err) => {
     if (err) {
       return console.log(err);
     }
 
-    res.redirect(CLIENT_HOME_PAGE_URL);
+    res.redirect(CLIENT_HOME_PAGE);
   });
-})
-
+});
 
 //Authorize with access token
-router.get('/reddit/callback', (req, res, next) => {
-    const requestToken = req.query.code;
-    
-    snoowrap.fromAuthCode({
+router.get("/reddit/callback", (req, res, next) => {
+  const requestToken = req.query.code;
+
+  snoowrap
+    .fromAuthCode({
       code: requestToken,
-      userAgent: 'scheddit',
+      userAgent: "scheddit",
       clientId: CLIENT_ID,
       clientSecret: CLIENT_SECRET,
-      redirectUri: 'http://localhost:8081/auth/reddit/callback'
-    }).then(response => {
+      redirectUri: REDIRECT_URI,
+    })
+    .then((response) => {
       const refreshToken = response.refreshToken;
-      const accessToken = response.access_token
+      const accessToken = response.access_token;
       console.log(refreshToken);
       req.session.token = accessToken;
       req.session.refresh = refreshToken;
@@ -66,8 +65,8 @@ router.get('/reddit/callback', (req, res, next) => {
       //Save sessionID to DB.
       //Session pesistence.
       //Send user info to client.
-      //Display logged in on client side. 
-      response.getMe().then(a => {
+      //Display logged in on client side.
+      response.getMe().then((a) => {
         //getMe method gets user object from snoowrap
         //Set currentUser to reddit username
         const currentUser = a.name;
@@ -76,26 +75,25 @@ router.get('/reddit/callback', (req, res, next) => {
         //userServices.findOrCreate(currentUser, accessToken);
         //Redirects to client home.
         req.session.userName = currentUser;
-        res.redirect(CLIENT_HOME_PAGE_URL);
+        res.redirect(CLIENT_HOME_PAGE);
       });
-      
-    })
+    });
+});
+
+//GENERATE AUTH URL
+router.get("/reddit", (req, res) => {
+  //Creates URL where users can authenticate via reddit
+  //If user chooses "Allow" then they are redirected to redirectUri
+  let authenticationUrl = snoowrap.getAuthUrl({
+    clientId: CLIENT_ID,
+    scope: ["identity", "read", "mysubreddits", "history"],
+    //Change to false. Will last an hour.
+    permanent: false,
+    redirectUri: REDIRECT_URI,
+    state: "teststring",
   });
 
-  //GENERATE AUTH URL
-router.get('/reddit', (req, res) => {
-    //Creates URL where users can authenticate via reddit
-    //If user chooses "Allow" then they are redirected to redirectUri
-    let authenticationUrl = snoowrap.getAuthUrl({
-      clientId: CLIENT_ID,
-      scope: ['identity', 'read', 'mysubreddits', 'history'],
-      //Change to false. Will last an hour.
-      permanent: false,
-      redirectUri: 'http://localhost:8081/auth/reddit/callback',
-      state: 'teststring'
-    })
-
-    res.redirect(authenticationUrl);
-  })
+  res.redirect(authenticationUrl);
+});
 
 module.exports = router;
