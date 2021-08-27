60985572bd89a1745d1fb5a1cda1ad5397f5d317
chaseharbour <harbourcp@gmail.com>
2021-08-27 16:10:00 -0700

    fix(VideoModal.js): Fixed video sizing. Sould no longer overflow and should close properly on mobile.

--- client/src/Components/Video.js
+++ client/src/Components/Video.js
@@ -3,12 +3,11 @@ import React from "react";
 const Video = ({ videoSrc }) => {
   return (
     <video
+      className="video"
       cross-origin="anonymous"
       muted
       autoplay="true"
       controls
-      width="100%"
-      height="100%"
     >
       <source src={videoSrc} type="video/mp4"></source>
     </video>
